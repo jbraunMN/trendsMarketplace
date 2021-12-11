62200a9330b2fba9052bad1a046fde29c0f03295
jbraunMN <95827809+jbraunMN@users.noreply.github.com>
2021-12-11 11:35:32 -0600

    Add files via upload

--- trendsMarketplace.py
+++ trendsMarketplace.py
@@ -2,14 +2,19 @@ import streamlit as st
 import mysql.connector as sql
 import pandas as pd
 import matplotlib
+from matplotlib.ticker import ScalarFormatter
 import matplotlib.pyplot as plt
 
 
 title = st.container()
 dbdesc = st.container()
+topProductLine = st.container()
 topProducts = st.container()
 topCustomer = st.container()
 salesEmployee = st.container()
+topCity = st.container()
+topVendor = st.container()
+shipping = st.container()
 
 
 
@@ -29,7 +34,7 @@ with dbdesc:
     
     
 with topProducts:
-    st.header('Top products in selected time period:')
+    st.header('Top products in selected time period')
     
     yearselect = st.selectbox(label = 'Select year to evaluate',
                               options = [2003, 2004, 2005])
@@ -71,7 +76,7 @@ with topProducts:
     plt.subplot(1,1,1)
     plt.style.use('ggplot')
     df[measure].plot.barh(legend = False)
-    plt.title('Top 10 Products')
+    # plt.title('Top 10 Products')
     plt.ylabel('Product Name')
     plt.xlabel('Product Sales in {0}'.format(measure))
     current_values = plt.gca().get_xticks()
@@ -79,6 +84,8 @@ with topProducts:
     
     
 with topCustomer:
+    st.header('Top 10 Customers per Year')
+    
     # select year period
     year = st.selectbox(label = 'Select the Year',
                         options = [2003, 2004, 2005])
@@ -99,13 +106,13 @@ with topCustomer:
     plt.subplot(1,1,1)
     plt.style.use('ggplot')
     final[measure].plot.bar(legend=False)
-    plt.title('Top 10 Customers')
+    # plt.title('Top 10 Customers')
     st.pyplot(fig1)
     
     
 
 with salesEmployee:
-    st.header('Sales under each employee at any given level: ')
+    st.header('Sales under each employee at any given level')
     
     empJobTitles = pd.read_csv('jgjobtitles.csv')
     empJobTitles[empJobTitles['jobTitle'].str.contains('Manager')] = 'Sales Manager'
@@ -139,10 +146,78 @@ with salesEmployee:
     plt.subplot(1,1,1)
     plt.style.use('ggplot')
     plt.bar(df1['emp_name'], height = df1['total_sales'])
-    plt.title('Sales under employee')
+    # plt.title('Sales under employee')
     plt.ylabel('Sales (in thousands)')
     plt.xlabel('Employee name')
     plt.xticks(rotation=90)
     current_values = plt.gca().get_xticks()
     st.pyplot(fig1)
 
+with topProductLine:
+    st.title('Sales performance:')
+    st.header('Most popular product lines')
+    
+    bdf1 = pd.read_csv('bProductLine.csv')
+    bdf1 = bdf1.sort_values('Total', ascending = False)
+    bdf1 = bdf1.set_index('Product_line')
+    
+    fig1 = plt.figure()
+    plt.subplot(1,1,1)
+    plt.style.use('ggplot')
+    plt.bar(bdf1.index, height = bdf1['Total'])
+    plt.gca().get_yaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
+    plt.xticks(rotation = 90)
+    #https://stackoverflow.com/questions/46735745/how-to-control-scientific-notation-in-matplotlib
+    st.pyplot(fig1)
+    
+with topCity:
+    st.header('Cities with the Highest Total Transactions')
+    bdf2 = pd.read_csv('bCity.csv')
+    bdf2 = bdf2.set_index('city')
+    bdf2 = bdf2[0:8]
+    
+    fig1 = plt.figure()
+    plt.subplot(1,1,1)
+    plt.style.use('ggplot')
+    plt.bar(bdf2.index, height = bdf2['Sales'])
+    plt.gca().get_yaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
+    plt.xticks(rotation = 90)
+    st.pyplot(fig1)
+    
+with topVendor:
+    st.title('Vendor Performance:')
+    st.header('Top 5 Vendors by Order Volume')
+    bdf3 = pd.read_csv('bVendors.csv')
+    bdf3 = bdf3.set_index('productVendor')
+    bdf3 = bdf3[0:5]
+    
+    fig1 = plt.figure()
+    plt.subplot(1,1,1)
+    plt.style.use('ggplot')
+    plt.bar(bdf3.index, height = bdf3['Orders'])
+    plt.gca().get_yaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
+    plt.xticks(rotation = 90)
+    st.pyplot(fig1)
+    
+with shipping:
+    st.header('Vendors with Shortest Average of Days to Ship')
+    bdf4 = pd.read_csv('bShipping.csv')
+    bdf4 = bdf4.groupby('productVendor').mean()
+    bdf4 = bdf4.sort_values('Days')
+    bdf4 = bdf4[0:4]
+    
+    fig1 = plt.figure()
+    plt.subplot(1,1,1)
+    plt.style.use('ggplot')
+    plt.bar(bdf4.index, height = bdf4['Days'])
+    plt.gca().get_yaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
+    plt.xticks(rotation = 90)
+    st.pyplot(fig1)
+    
+
+    
+    
+    
+    
+    
+    
