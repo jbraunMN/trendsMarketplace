f4ac621e9c34cb39d25ca27f19a67e9e907d0288
jbraunMN <95827809+jbraunMN@users.noreply.github.com>
2021-12-13 21:47:01 -0600

    Update trendsMarketplace.py

--- trendsMarketplace.py
+++ trendsMarketplace.py
@@ -118,7 +118,7 @@ with salesEmployee:
     empJobTitles[empJobTitles['jobTitle'].str.contains('Manager')] = 'Sales Manager'
     jobTitles = pd.Series(empJobTitles['jobTitle'].unique())
     # Create a selectbox to select the position of employee
-    title_list = empJobTitles['jobTitle'].to_list()
+    title_list = pd.Series(empJobTitles['jobTitle'].unique()).to_list()
     default_ix = title_list.index("Sales Manager")
     title = st.selectbox(label = "Job title of the employee",
                       options = title_list, index = default_ix) 
